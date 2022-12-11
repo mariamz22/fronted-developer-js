5bd66e3825f50406d537b86dc17411ef0bfaf50a
Maria Mendoza Zafra <mmendozazafra@gmail.com>
2022-12-10 19:07:30 -0500

    Open product info of each element
    Abrir la información de cada elemento en el product-detail-aside.
    Cerrar los menús que no son necesarios.

--- main.js
+++ main.js
@@ -9,7 +9,10 @@ const mobileMenu = document.querySelector('.mobile-menu');
 //Resumen de shopping-cart
 const shoppingCartIcon = document.querySelector('.navbar-shopping-cart')
 const shoppingCartResume = document.querySelector('.product-detail')
+
+//Menu Product-detail
 const productDetail = document.querySelector('.product-detail-aside')
+const asideClose = document.querySelector('.product-detail-close')
 
 //Menu Principal
 const cardsContainer = document.querySelector('.cards-container');
@@ -37,53 +40,79 @@ productList.push({
 navEmail.addEventListener('click', showDesktopMenu);
 menu.addEventListener('click', showMobileMenu);
 shoppingCartIcon.addEventListener('click', showShoppingCart);
+asideClose.addEventListener('click', closeAside);
 renderProducts(productList);
 
 
 function renderProducts(arr){
-    for(product of arr){
+    
+    arr.forEach(product => {
         const productCard = document.createElement('div');
         productCard.classList.add('product-card');
-    
+        productCard.addEventListener('click',()=>{ //evento de escucha
+            openProductInfo(product); //llamo a la funcion
+        })              
+        
         const productImg = document.createElement('img');
-        productImg.setAttribute('src', product.image)
-        // productImg.addEventListener('click', openProductDetail);
-    
+        productImg.setAttribute('src', product.image) 
+        
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
-    
+        
         const productInfoDiv = document.createElement('div');
-    
+        
         const productPrice = document.createElement('p');
         productPrice.innerText = '$ ' + product.price;
-    
+        
         const productName = document.createElement('p');
         productName.innerText = product.name;
-    
+        
         productInfoDiv.append(productName, productPrice);
-    
+        
         const productInfoFigure = document.createElement('figure');
         const productImgCart = document.createElement('img');
         productImgCart.setAttribute('src', './assets/icons/bt_add_to_cart.svg');
-    
+        
         productInfoFigure.appendChild(productImgCart);
         productInfo.append(productInfoDiv, productInfoFigure);
         productCard.append(productImg, productInfo);
         cardsContainer.appendChild(productCard);
+        
+    });
     
+    function openProductInfo(product){
+
+        shoppingCartResume.classList.add('inactive');
+        
+        const aside = document.querySelector('.product-detail-aside');
+        const productImg = document.querySelector('.product-detail-aside .aside-img');
+        const productPrice = document.querySelector('.product-info-aside .product-price');
+        const productName= document.querySelector('.product-info-aside .product-name');
+        aside.classList.remove('inactive');
+        productImg.setAttribute('src',product.image);
+        productPrice.textContent= '$ ' + product.price;
+        productName.textContent= product.name;
     }
+    
+}
+
+function closeAside(){
+    productDetail.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    
 }
 
 function showDesktopMenu() {
     shoppingCartResume.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
+    productDetail.classList.add('inactive');
 }
-
 
 function showMobileMenu() {
     shoppingCartResume.classList.add('inactive');
     desktopMenu.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
+    productDetail.classList.add('inactive');
 }
 
 function showShoppingCart() {
@@ -92,5 +121,4 @@ function showShoppingCart() {
     productDetail.classList.add('inactive');
     shoppingCartResume.classList.toggle('inactive');
 }
-
 
